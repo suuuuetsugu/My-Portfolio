1b9e8fee53214698160cc76b6753f02b15dc0f94
Nao Suetsugu <102156963+suuuuetsugu@users.noreply.github.com>
2022-10-20 18:28:09 +0900

    Delete .history directory

--- .history/backend/routes/users_20220823232615.ts
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req: any, res: any, next: any) {
-  res.send('かきくけこ');
-});
-
-module.exports = router;
--- .history/server/routes/users_20220823215833.ts
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-
-export const router = Router();
-
-router.get("/", (req, res, next) => res.send('アイウエオ'));
